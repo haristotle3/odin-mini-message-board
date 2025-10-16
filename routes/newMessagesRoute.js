e6870a1a2cf6fd6d03da8e246ffcc92d021c2312
haristotle <haristotlepatil2003@gmail.com>
2025-10-16 15:48:47 +0530

    Implement route handler for form submission post request

--- routes/newMessagesRoute.js
+++ routes/newMessagesRoute.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { messages } from "./index.js";
 
 const newMessageRouter = Router();
 
 newMessageRouter.get("/", (req, res) => {
   res.render("form");
+});
+
+newMessageRouter.post("/", (req, res) => {
+  const newMessageObject = { ...req.body, added: new Date() };
+  messages.push(newMessageObject);
+  res.redirect("/");
 });
 
 export default newMessageRouter;
