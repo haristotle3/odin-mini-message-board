e6d9fccfd6c14b4afbe6d293d79084974cb13d5a
haristotle <haristotlepatil2003@gmail.com>
2025-10-16 16:09:10 +0530

    Move messages from index route to models directory

---
+++ models/messages.js
@@ -0,0 +1,24 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "How’s everyone doing today?",
+    user: "Samantha",
+    added: new Date(),
+  },
+  {
+    text: "Just finished working on my Express app!",
+    user: "Daniel",
+    added: new Date(),
+  },
+];
+
+export default messages;
