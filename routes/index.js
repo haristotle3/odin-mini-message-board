04b78c98e361d5238155d5ba591aedb489e9b568
haristotle <haristotlepatil2003@gmail.com>
2025-10-16 13:46:36 +0530

    Implement index router

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "How’s everyone doing today?",
+    user: "Samantha",
+    added: new Date(),
+  },
+  {
+    text: "Just finished working on my Express app!",
+    user: "Daniel",
+    added: new Date(),
+  },
+];
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Odin Mini Message Board", messages: messages });
+});
+
+export default indexRouter;
