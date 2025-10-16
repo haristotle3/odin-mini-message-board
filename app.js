83ecf2d0b79f455b3cebfe94a2e7534a91f0846e
haristotle <haristotlepatil2003@gmail.com>
2025-10-16 13:52:15 +0530

    Import and use newMessageRouter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "path";
 import "dotenv/config";
 import { fileURLToPath } from "url";
 import indexRouter from "./routes/index.js";
+import newMessageRouter from "./routes/newMessagesRoute.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,9 +15,11 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", indexRouter);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 
 app.listen(PORT, hostname, () => {
   console.log(`Server running on ${hostname}:${PORT}`);
